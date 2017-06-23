982e80fa1332c830008cb0416b934f614a9f98dd
Kenneth Hansen <kenneth@khansen-it.dk>
2017-06-23 13:16:39 +0200

    Fixed a few issues with base and home view

--- djangoshop/urls.py
+++ djangoshop/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from store.views.home_page_view import HomePageView
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
+    #url(r'^accounts/', include('registration.backends.default.urls')),
 ]
