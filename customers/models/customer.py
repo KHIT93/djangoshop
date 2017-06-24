70454caa75589a80679ec5ce97718aacb9325fac
Kenneth Hansen <kenneth@khansen-it.dk>
2017-06-24 14:41:27 +0200

    Altered the customer model to allow for optional values on the address information

--- customers/models/customer.py
+++ customers/models/customer.py
@@ -4,8 +4,8 @@ from django.conf import settings
 class Customer(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     address1 = models.CharField(max_length=255)
-    address2 = models.CharField(max_length=255)
-    address3 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    address3 = models.CharField(max_length=255, blank=True, null=True)
     zip_code = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, blank=True, null=True)
     country = models.CharField(max_length=255)
