d7962be9f1e1cd10b2acf9a286d514a62fe7699c
Kenneth Hansen <kenneth@khansen-it.dk>
2017-06-27 08:56:13 +0200

    Updated production.py to contain credentials for staging server

--- djangoshop/settings/production.py
+++ djangoshop/settings/production.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'djangoshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoshop',
+        'USER': 'djangoshop',
+        'PASSWORD': 'Django99##',
+        'HOST': '172.16.240.6',
+        'PORT': '3306',
     }
 }
 
